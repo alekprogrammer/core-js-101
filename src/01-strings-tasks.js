9420af3c1f54929f7bd1796b4837fd8392b9a84f
Aleksandr Lobkov <das149848@gmail.com>
2022-07-23 18:54:32 +0400

    feat: complete some tasks

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -18,8 +18,8 @@
  *   'aa',''    => 'aa'
  *   '',  'bb'  => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  return value1 + value2;
 }
 
 
@@ -34,8 +34,8 @@ function concatenateStrings(/* value1, value2 */) {
  *   'b'     => 1
  *   ''      => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(value) {
+  return value.length;
 }
 
 /**
@@ -51,8 +51,8 @@ function getStringLength(/* value */) {
  *   'John','Doe'      => 'Hello, John Doe!'
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
@@ -65,8 +65,8 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value.slice(7, value.length - 1);
 }
 
 
@@ -80,8 +80,8 @@ function extractNameFromTemplate(/* value */) {
  *   'John Doe'  => 'J'
  *   'cat'       => 'c'
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  return value[0];
 }
 
 /**
@@ -95,8 +95,9 @@ function getFirstChar(/* value */) {
  *   'cat'              => 'cat'
  *   '\tHello, World! ' => 'Hello, World!'
  */
-function removeLeadingAndTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeLeadingAndTrailingWhitespaces(value) {
+  const strWithoutSpaces = value.replace(/(^[\s\t]*)([a-zA-Z0-9!, ]*[a-zA-Z0-9!,])([\s\t]*$)/, '$2');
+  return strWithoutSpaces;
 }
 
 /**
@@ -110,8 +111,8 @@ function removeLeadingAndTrailingWhitespaces(/* value */) {
  *   'A', 5  => 'AAAAA'
  *   'cat', 3 => 'catcatcat'
  */
-function repeatString(/* value, count */) {
-  throw new Error('Not implemented');
+function repeatString(value, count) {
+  return value.repeat(count);
 }
 
 /**
@@ -126,8 +127,8 @@ function repeatString(/* value, count */) {
  *   'I like legends', 'end' => 'I like legs',
  *   'ABABAB','BA' => 'ABAB'
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  return str.replace(value, '');
 }
 
 /**
@@ -141,8 +142,8 @@ function removeFirstOccurrences(/* str, value */) {
  *   '<span>' => 'span'
  *   '<a>' => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.slice(1, str.length - 1);
 }
 
 
@@ -156,8 +157,8 @@ function unbracketTag(/* str */) {
  *   'Thunderstruck' => 'THUNDERSTRUCK'
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
-function convertToUpperCase(/* str */) {
-  throw new Error('Not implemented');
+function convertToUpperCase(str) {
+  return str.toUpperCase();
 }
 
 /**
@@ -175,8 +176,8 @@ function convertToUpperCase(/* str */) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  return str.split(';');
 }
 
 /**
@@ -202,8 +203,30 @@ function extractEmails(/* str */) {
  *             '└──────────┘\n'
  *
  */
-function getRectangleString(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleString(width, height) {
+  let rectangle = '';
+  for (let i = 0; i < height; i += 1) {
+    for (let j = 0; j <= width; j += 1) {
+      if (i === 0 && j === 0) {
+        rectangle += '┌';
+      } else if ((i === 0 || i === height - 1) && j > 0 && j < width - 1) {
+        rectangle += '─';
+      } else if ((j === 0 || j === width - 1) && i > 0 && i < height - 1) {
+        rectangle += '│';
+      } else if ((i > 0 || i < height - 1) && j > 0 && j < width - 1) {
+        rectangle += ' ';
+      } else if (i === 0 && j === width - 1) {
+        rectangle += '┐';
+      } else if (i === height - 1 && j === 0) {
+        rectangle += '└';
+      } else if (i === height - 1 && j === width - 1) {
+        rectangle += '┘';
+      } else if (j === width) {
+        rectangle += '\n';
+      }
+    }
+  }
+  return rectangle;
 }
 
 
@@ -223,8 +246,29 @@ function getRectangleString(/* width, height */) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  let encodestr = '';
+  const Alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  for (let i = 0; i < str.length; i += 1) {
+    if (/\W/.test(str[i])) {
+      encodestr += str[i];
+    } else if (str[i] === str[i].toUpperCase()) {
+      const indexOfCurrChar = Alphabet.findIndex((elem) => elem === str[i].toLowerCase());
+      if (indexOfCurrChar < 13) {
+        encodestr += Alphabet[indexOfCurrChar + 13].toUpperCase();
+      } else {
+        encodestr += Alphabet[indexOfCurrChar - 13].toUpperCase();
+      }
+    } else {
+      const indexOfCurrChar = Alphabet.findIndex((elem) => elem === str[i]);
+      if (indexOfCurrChar < 13) {
+        encodestr += Alphabet[indexOfCurrChar + 13];
+      } else {
+        encodestr += Alphabet[indexOfCurrChar - 13];
+      }
+    }
+  }
+  return encodestr;
 }
 
 /**
@@ -240,8 +284,8 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return typeof value === 'string' || value instanceof String;
 }
 
 
@@ -269,8 +313,37 @@ function isString(/* value */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const indexesOfChars = {
+    A: 0, J: 10, Q: 11, K: 12,
+  };
+  let index;
+  if (value[value.length - 1] === '♣') {
+    if (/\D/.test(value.slice(0, value.length - 1))) {
+      index = Number(value.slice(0, value.length - 1)) - 1;
+    } else {
+      index = indexesOfChars[value[0]];
+    }
+  } else if (value[value.length - 1] === '♦') {
+    if (/\D/.test(value.slice(0, value.length - 1))) {
+      index = Number(value.slice(0, value.length - 1)) + 12;
+    } else {
+      index = indexesOfChars[value[0]] + 13;
+    }
+  } else if (value[value.length - 1] === '♥') {
+    if (/\D/.test(value.slice(0, value.length - 1))) {
+      index = Number(value.slice(0, value.length - 1)) + 25;
+    } else {
+      index = indexesOfChars[value[0]] + 26;
+    }
+  } else if (value[value.length - 1] === '♠') {
+    if (/\D/.test(value.slice(0, value.length - 1))) {
+      index = Number(value.slice(0, value.length - 1)) + 38;
+    } else {
+      index = indexesOfChars[value[0]] + 39;
+    }
+  }
+  return index;
 }
 
 
--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -19,8 +19,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
 
 
@@ -35,8 +35,8 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return 2 * Math.PI * radius;
 }
 
 /**
@@ -51,8 +51,11 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  if (value1 - value2 === 0) {
+    return 0;
+  }
+  return Math.abs(value1 - value2) / 2 + Math.min(value1, value2);
 }
 
 /**
@@ -70,8 +73,10 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const distancex = x1 - x2;
+  const distancey = y1 - y2;
+  return Math.sqrt(distancex * distancex + distancey * distancey);
 }
 
 /**
@@ -86,8 +91,11 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  if (a === 0 || b === 0) {
+    return 0;
+  }
+  return (-b) / a;
 }
 
 
@@ -125,8 +133,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const strvalue = String(value);
+  return Number(strvalue[strvalue.length - 1]);
 }
 
 
@@ -141,8 +150,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return String(value);
 }
 
 /**
@@ -180,8 +189,8 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / (10 ** pow)) * (10 ** pow);
 }
 
 /**
@@ -201,8 +210,19 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 2 || n === 3) {
+    return true;
+  }
+  if (n <= 1 || n % 2 === 0 || n % 3 === 0) {
+    return false;
+  }
+  for (let i = 5; i * i <= n; i += 6) {
+    if (n % i === 0 || n % (i + 2) === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -220,8 +240,11 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (Number.isNaN(Number(value))) {
+    return def;
+  }
+  return value;
 }
 
 module.exports = {
